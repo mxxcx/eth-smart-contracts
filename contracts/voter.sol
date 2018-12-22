ae4f8e65e53c60efb48d0f3d126577a83f6937c2
mtdx <ms2asdk@gmail.com>
2018-12-22 12:13:11 +0200

    use string options

--- contracts/voter.sol
+++ contracts/voter.sol
@@ -3,13 +3,24 @@ pragma experimental ABIEncoderV2;
 
 contract Voter {
 
+    struct OptionPos {
+        uint pos;
+        bool exists;
+    }
+
     uint[] public votes;
     mapping (address => bool) hasVoted;
+    mapping (string => OptionPos) posOfOption;
     string[] public options;
 
     constructor(string[] _options) public {
         options = _options;
         votes.length = options.length;
+
+        for (uint i = 0; i < options.length; i++) {
+            OptionPos memory option = OptionPos(i, true);
+            posOfOption[options[i]] = option;
+        }
     }
 
     function vote(uint option) public {
@@ -18,6 +29,15 @@ contract Voter {
 
         hasVoted[msg.sender] = true;
         votes[option] = votes[option] + 1;
+    }
+
+    function vote(string option) public {
+        require(!hasVoted[msg.sender], "Account has already voted");
+        OptionPos memory optionPos = posOfOption[option];
+        require(optionPos.exists, "Option does not exist");
+
+        hasVoted[msg.sender] = true;
+        votes[optionPos.pos] = votes[optionPos.pos]++;
     }
 
     function getOptions() public view returns (string[]) {
